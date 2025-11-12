e49629607d218b939e21ea723d5f30414f13e25e
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:14:19 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -6,6 +6,28 @@ using namespace std;
 
 //define animal class here 
 
+class Animal(){
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    Animal();
+    Animal(string name, int age, bool isHungry);
+   virtual ~Animal();
+    
+void display();
+void feed();
+
+string getname(){return name;}
+int getage(){return age;}
+bool getisHungry(){return isHungry;}
+void setname(){const name;}
+void setage(){const age;}
+void setisHungry(){const isHungry;}
+};
+
 
 
 #endif
