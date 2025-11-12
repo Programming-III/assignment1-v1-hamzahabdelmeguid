3b2d74a6d3b2e6ef8dbbc76011fe339217f6455a
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:37:06 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,22 +5,22 @@
 
 //define reptile class here
 
-class Mammal : public Animal {
+class Reptile : public Animal {
 private:
-    string furColor;
+    bool isVenomous;
 public:
-    Mammal() : Animal(){
-       
-    }
-    Mammal(string name, int age, bool isHungry, string furColor) : Animal(name, age, isHungry){
-       
-    }
-    virtual ~Mammal(){}
-    void display(){
-        
+    Reptile() : Animal(){
         
     }
-};
+    Reptile(string name, int age, bool isHungry, bool isVenomous) : Animal(name, age, isHungry){
+        
+    }
+    virtual ~Reptile(){}
+    void display(){}
+      
+        
+        
+ 
 
 
 
