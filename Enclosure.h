ebb1c21c276f8c8404eea49697e050a4fd579de9
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:15:35 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -4,6 +4,28 @@
 #include "Animal.h"
 
 //define enclosure class here 
+class Enclosure(): class Animal(){
+private:
+    Animal* animal:
+        int capacity;
+        int currentcount;
+
+public:
+    Enclosure();
+    Enclosure(Animal* animal, int capacity, int currentcount);
+   virtual ~Enclosure();
+    int getcapacity(){return capacity;}
+    int getcurrentcount(){return currentcount;}
+    
+    void setcapacity(){const capacity;}
+    void setcurrentcount(){const currentcount;}
+    
+    void addAnimal(Animal* animal);
+    void displayAnimals();
+    
+
+
+};
 
 
 
