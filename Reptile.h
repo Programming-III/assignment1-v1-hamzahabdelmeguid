97cef9058221a01f2453e72f516949ba12cc3359
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:16:57 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,6 +5,19 @@
 
 //define reptile class here
 
+class Reptile : public Animal
+{
+    public:
+        Reptile();
+        Reptile(bool isVenomous);
+        virtual ~Reptile();
+
+        unsigned bool GetisVenomous() { return isVenomous; }
+        void SetisVenomous() { const isVenomous; }
+
+    private:
+    bool isVenomous;
+};
 
 
 
