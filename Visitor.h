892664aec8714f1ab901dfee13accfd7467e9611
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:18:43 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,6 +5,21 @@
 using namespace std;
 
 //define visitor class here
+class Visitor(){
+private:
+    string visitorname;
+    int tickietsbought;
+public:
+    Visitor();
+    Visitor(string visitorname, int ticketsbought);
+   virtual ~visitor();
+   
+   string getvisitorname(){return visitorname;}
+   int getticketsbought(){return ticketsbought}
+   void setvisitorname(){const visitorname;}
+   void setticketsbought(){const ticketsbought;}
+void displayInfo();
+};
 
 
 
