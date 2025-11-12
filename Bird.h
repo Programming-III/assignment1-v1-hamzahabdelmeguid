128252d0b7ee97f864bbe6e6fac026178fee81c1
hamzahabdelmeguid <hamza.abdelmeguid@student.giu-uni.de>
2025-11-12 15:15:08 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -4,7 +4,17 @@
 #include "Animal.h"
 
 //define class bird here 
+class Bird : public Animal
+{
+     private:
+         float wingSpan;
+    public:
+        Bird();
+        Bird(float wingSpan);
+        virtual ~Bird();
 
+   
+};
 
 
 
